ab241bff8f24e7d61d539aa2d48d49690d36c8f6
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2024-01-29 18:34:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os 
 
-TOKEN = os.environ.get("TOKEN", "6318256885:AAGgylgIY6zZXlegJfFdJO5QJOogSFhQAxs")
+TOKEN = os.environ.get("TOKEN", "6318256885:AAFvBL_LqoPm0rXavgsegREqnwRxNCqTAvM")
 OWNER = os.environ.get("OWNER", "SaravanaKrish")
 GROUP = os.environ.get("GROUP", "TamilChatGroup")
 CHANNEL = os.environ.get("CHANNEL", "TamilBots")
