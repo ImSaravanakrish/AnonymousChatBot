b37b9ee8bfa967f15ee8e6339badd84008c59df3
Krish004 <95554021+Krish004@users.noreply.github.com>
2024-01-29 12:19:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
 
 TOKEN = os.environ.get("TOKEN", "6318256885:AAGgylgIY6zZXlegJfFdJO5QJOogSFhQAxs")
-OWNER = os.environ.get("OWNER", "useless07")
+OWNER = os.environ.get("OWNER", "SaravanaKrish")
 GROUP = os.environ.get("GROUP", "TamilChatGroup")
 CHANNEL = os.environ.get("CHANNEL", "TamilBots")
 BOT_NAME = os.environ.get("BOT_NAME", "Anonymous ChatBot")
