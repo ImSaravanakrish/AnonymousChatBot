3d09b097059cb4a1f546024662f7fe03e0026196
Krish004 <95554021+Krish004@users.noreply.github.com>
2024-01-29 13:57:09 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -35,7 +35,7 @@ botton=[[
       InlineKeyboardButton("Support Group 👥", url="https://t.me/TamilSupport"),
       InlineKeyboardButton("Updates Channel 📢", url="https://t.me/TamilBots")
       ],[
-      InlineKeyboardButton("Bot List 🤖", url=f"https://t.me/TamilBots/84")
+      InlineKeyboardButton("Tamil Chat Group", url=f"https://TamilChatGroup")
       ]]
 
 user_dict = {}  
@@ -53,7 +53,7 @@ def welcome(message):
         mark = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
         mark.add('🔍 Find a Partner')
         mark.add('📰 Info Profile', '🗑 Delete Profile')
-        bot.send_message(message.from_user.id, f"*Welcome to {BOT_NAME}🙊*\n\n_Hope you get a friend or a mate_\n\n*NOTE:*\nJOIN\n[👥 ɢʀᴏᴜᴘ](t.me/{GROUP}) | [ᴄʜᴀɴɴᴇʟ 📣](t.me/{CHANNEL}) | [📱ᴏᴡɴᴇʀ](t.me/{OWNER})",reply_markup=InlineKeyboardMarkup(botton), disable_web_page_preview=True)
+        bot.send_message(message.from_user.id, f"*Welcome to {BOT_NAME}🙊*\n\n_Hope you get a friend or a mate_\n\n*NOTE:*\nJOIN\n[👥 ɢʀᴏᴜᴘ](t.me/{GROUP}) | [ᴄʜᴀɴɴᴇʟ 📣](t.me/{CHANNEL}) | [📱ᴏᴡɴᴇʀ](t.me/{OWNER})",reply_markup=InlineKeyboardMarkup(botton), disable_web_page_preview=True,parse_mode='html')
         bot.register_next_step_handler(message, search_prof)
     else:
         bot.send_message(message.from_user.id, "_👋Hello New Users, To Continue Filling The Following Bio data!_",reply_markup=InlineKeyboardMarkup(buton), disable_web_page_preview=True)
